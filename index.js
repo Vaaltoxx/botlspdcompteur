58236177d77b6b00b0f88556b469197233bdc232
Vaaltoxx <90828723+Vaaltoxx@users.noreply.github.com>
2021-12-29 07:55:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const config = require("./config.json");
+const Discord = require("discord.js");
+const chalk = require('chalk');
+const gamedig = require("gamedig");
+
+const client = new Discord.Client();
+
+client.config = config;
+
+var nb = 1000
+
+
+
+const updateChannel = async () => {
+
+		const channel = client.channels.cache.get(config.playerCountChannelID);
+		if(!channel) throw new Error("La salon spécifié dans la configuration n'existe pas !");
+		
+	    const stats = await gamedig.query({
+	        type: "garrysmod",
+	        host: config.playerCountServerIP,
+	        port: config.playerCountServerPort
+		});
+		
+	    if (stats.raw.numplayers != nb) {
+	    	channel.setName(`👥 ・GMod4Life : ${stats.raw.numplayers}/90`);
+			client.user.setActivity(`👥 ・GMod4Life : ${stats.raw.numplayers}/90`, {type: "PLAYING"});
+			client.user.setStatus('dnd')
+	    }
+};
+
+client.on("ready", () => {
+
+	console.log(chalk.green("[BOT] Connected to Discord."));
+	
+	
+	updateChannel();
+	setInterval(updateChannel, 60000*0.05);
+	
+});
+
+client.login(process.env.TOKEN);
